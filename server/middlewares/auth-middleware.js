a28a3a4d2a3b7e78ca22045c2ceb7306c09a40d9
mtx-dev <andrey.kozlitskiy@gmail.com>
2021-12-21 19:50:17 +0200

    add auth middleware

---
+++ server/middlewares/auth-middleware.js
@@ -0,0 +1,27 @@
+const ApiError = require('../exeptions/api-error');
+const tokenService = require('../service/token-service')
+
+module.exports = function (req, res, next) {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        if (!authorizationHeader) {
+            next(ApiError.UnauthorizedError());
+        }
+        
+        const accessToken = authorizationHeader.split(' ')[1];
+        if (!accessToken) {
+            next(ApiError.UnauthorizedError());
+        }
+        
+        const userData = tokenService.validationAccessToken(accessToken);
+        if (!userData) {
+            next(ApiError.UnauthorizedError());
+        }
+
+        req.user = userData;
+        next();
+        
+    } catch (e) {
+        return next(ApiError.UnauthorizedError());
+    }
+}
