1d8e5a5fd49ae11dbd46c1a1372e5cf95b44177a
mtx-dev <andrey.kozlitskiy@gmail.com>
2022-01-11 22:33:59 +0200

    routes

---
+++ client/src/hoocks/useRoutes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import CabinetSection from '../sections/CabinetSection';
+import MainSection from '../sections/MainSection';
+import NotFoundSection from '../sections/NotFoundSection';
+import VocabularySection from '../sections/VocabularySection';
+
+export default function useRoutes(isAuthenticated) {
+    console.log(isAuthenticated);
+    return(
+        <Routes>  
+            <Route path='/user'  element={ <VocabularySection />}>
+                <Route path='voabulary'  element={ <VocabularySection />} />
+                <Route path='cabinet'  element={ <CabinetSection />} />
+            </Route>
+            <Route path='/'  element={ <MainSection />} />
+            <Route path='*'  element={ <NotFoundSection />} />
+        </Routes>
+    )   
+}
