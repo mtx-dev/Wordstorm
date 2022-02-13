9040278e6ad9d713f48cb136ed1a9cf77a67995e
mtx-dev <andrey.kozlitskiy@gmail.com>
2022-02-13 19:00:56 +0200

    fix sections

--- client/src/sections/MainSection.tsx
+++ client/src/sections/MainSection.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import LoginForm from '../components/LoginForm';
+import UserService from '../services/UserServoce';
 
 export default function MainSection() {
+    const handl = async () => {
+        try {
+            const users = await UserService.getUsers();
+            console.log(users.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
-        <div>main
-        <LoginForm />
+        <div>
+            <button onClick={handl}>get</button>
+            main
         </div>
     );
 }
---
+++ client/src/sections/ScudSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function VocabularySection() {
+    return (
+        <div>scud</div>
+    );
+}
