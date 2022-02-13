30bd3e13e173567c7f9fd0abbdde188dbaf5696a
mtx-dev <andrey.kozlitskiy@gmail.com>
2022-02-13 19:05:07 +0200

    fix roures

--- client/src/hoocks/useRoutes.tsx
+++ client/src/hoocks/useRoutes.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import CabinetSection from '../sections/CabinetSection';
 import MainSection from '../sections/MainSection';
-import NotFoundSection from '../sections/NotFoundSection';
+// import NotFoundSection from '../sections/NotFoundSection';
 import VocabularySection from '../sections/VocabularySection';
+import ScudSection from '../sections/ScudSection';
+import Layout from '../components/Layout';
+import RequireAuth from '../components/RequireAuth';
+import LoginSection from '../sections/LoginSection';
+import NotFoundSection from '../sections/NotFoundSection';
 
 export default function useRoutes() {
     return(
-        <Routes>  
-            <Route path='/user'  element={ <VocabularySection />}>
-                <Route path='voabulary'  element={ <VocabularySection />} />
-                <Route path='cabinet'  element={ <CabinetSection />} />
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<MainSection />}/>
+                <Route path="login" element={<LoginSection />} />
+                <Route path="scud"
+                    element={<RequireAuth><ScudSection /></RequireAuth>} />
+                <Route path="cabinet"
+                    element={<RequireAuth><CabinetSection /></RequireAuth>} />
+                <Route path="vocabulary"
+                    element={<RequireAuth><VocabularySection /></RequireAuth>} />
+                <Route path="settings" 
+                    element={<RequireAuth><VocabularySection /></RequireAuth>} />
+                <Route path="*" element={<NotFoundSection />} />
             </Route>
-            <Route path='/'  element={ <MainSection />} />
-            <Route path='*'  element={ <NotFoundSection />} />
         </Routes>
     )   
 }
