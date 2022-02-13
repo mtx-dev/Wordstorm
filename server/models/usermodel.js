cff9ce7a287ce598cefc4d86fb40a1c327e87019
mtx-dev <andrey.kozlitskiy@gmail.com>
2022-02-13 19:09:21 +0200

    todo

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -3,7 +3,8 @@ const {Schema, model} = require('mongoose');
 const UserSchema = new Schema({
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
-    isActivated: {type: Boolean, default: false},
+    // TODO: realize Activations
+    isActivated: {type: Boolean, default: true},
     activationLink: {type: String},
 });
 
