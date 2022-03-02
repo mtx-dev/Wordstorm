590d1843b7a72e5c4c544ed76eb83e365a075bdd
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-03-02 16:12:22 +0200

    Add stat and voc to context

--- client/src/context/Context.ts
+++ client/src/context/Context.ts
@@ -1,8 +1,10 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 import { IUser } from "../models/IUser";
+import { IWord } from '../models/IWord';
 
 export type AuthInFunc = (email: string, password: string, callback?: VoidFunction) => Promise<void>;
 export type AuthOutFunc = (callback?: VoidFunction) => Promise<void>;
+export type SaveStatFunc = (statistc: IWord[]) => Promise<void>;
 
 export interface StoreContextType {
     isLoading: boolean;
@@ -11,6 +13,8 @@ export interface StoreContextType {
     login: AuthInFunc;
     registration: AuthInFunc;
     logout: AuthOutFunc;
+    vocabulary: IWord[];
+    saveStatistic: SaveStatFunc;
   }
 
 
