c0ecbfadf04d4584da756830cfa4a3a497778b21
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-03-22 22:16:08 +0200

    fix card

--- client/src/components/QuizCard.tsx
+++ client/src/components/QuizCard.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 
 const QuizCard = ({ children }: { children: JSX.Element }) => {
   
-    return children;
+    return (
+        <Row className="justify-content-center">
+            <Col className="col-4">
+                {children}
+            </Col>
+        </Row>
+    );
 }
 
 export default QuizCard;
