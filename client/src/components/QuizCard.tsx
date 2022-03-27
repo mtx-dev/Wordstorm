cb29b4cd2be72d124d7303849136df77717d8b1c
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-03-27 11:45:26 +0300

    Revork template

--- client/src/components/QuizCard.tsx
+++ client/src/components/QuizCard.tsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 
-const QuizCard = ({ children }: { children: JSX.Element }) => {
+const QuizCard = ({ children, disabledNext, handleNextWord, title, pazzle }: 
+        { 
+            children: JSX.Element | JSX.Element[];
+            disabledNext: boolean;
+            handleNextWord: () => void;
+            title: string;
+            pazzle:string
+        }) => {
   
     return (
         <Row className="justify-content-center">
             <Col className="col-4">
-                {children}
+            <Card bg='dark' className='p-3'>
+                <Card.Header>
+                    <Row>
+                        <Col>{title}</Col>
+                        <Col md='auto'>
+                            <Button 
+                                variant={disabledNext ? 'outline-secondary' : 'primary' }
+                                disabled={disabledNext}
+                                onClick={handleNextWord}
+                            >
+                                next
+                            </Button>
+                        </Col>
+                    </Row>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title className='p-2 text-light d-flex justify-content-center'>
+                        {pazzle}
+                    </Card.Title>
+                    {children}
+                </Card.Body>
+            </Card>
             </Col>
         </Row>
     );
