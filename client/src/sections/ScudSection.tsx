5802b69213565f2379ba3ab56ed9946a4dace417
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-04-05 23:08:34 +0300

    Rework

--- client/src/sections/ScudSection.tsx
+++ client/src/sections/ScudSection.tsx
@@ -1,16 +1,10 @@
-import React, { useState } from 'react';
-import QuizCard from '../components/QuizCard';
-import QuizListen from '../components/QuizListen';
-import QuizReverseTranslate from '../components/QuizReverseTranslate';
-import QuizSpell from '../components/QuizSpell';
-import QuizTranslate from '../components/QuizTranslate';
+import React, { useEffect, useState } from 'react';
 import useAsyncEffect from '../hoocks/useAsyncEffect';
 import useQuizes from '../hoocks/useQuizes';
 import useStatistic from '../hoocks/useStatistic';
 import useWords from '../hoocks/useWords';
-import { NextFunc } from '../models/IQuizProps';
 import { IWord } from '../models/IWord';
-import { IQuizResult } from '../models/StatisticTypes';
+import { shuffle } from '../utils/shuffle';
 
 const words: IWord[] = [
     {id: 1, word: 'home,   house', 
@@ -38,57 +32,77 @@ const words: IWord[] = [
     translation: 'dom8', 
     status: 'stydy', lastSuccessful: null,  attempts: 0, successfulAttempts: 0,  active: true,},
 ];
+enum Status {
+    Start,
+    Play,
+    End,
+}
 
 export default function ScudSection() {
 
     // const words = useWords();
-    // const quizes = useQuizes();
-    // const defaultStatistic = words.reduce((result, word) => {
-    //     result[word.id] = true;
-    //     return result;
-    // }, {});
+    const quizes = useQuizes();
 
-    // const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
-    // const [isFinish, setIsFinish] = useState(false);
-    // const [unsuccessfull, setUnsuccessfull] = useState(defaultStatistic);
-    // const { saveStatistic } = useStatistic();
-    // words from voca
-    // quizes from setings
+    const { saveStatistic } = useStatistic();
+    const defaultStatistic = words.reduce((result, word) => {
+        result[word.id] = true;
+        return result;
+    }, {} as Record<number, boolean>); 
 
-    const handleNext:NextFunc = (statistic) => {
-        // if (currentQuizIndex < quizes.length) {
-        //     setCurrentQuizIndex(currentQuizIndex + 1);
-        //     const newStatistic = {...unsuccessfull}
-        //     statistic.filter((word) => !word.success)
-        //         .forEach((word) => newStatistic[word.wordId] = word.success)
-        //     setUnsuccessfull(newStatistic);
-        //     return;
-        // }
-        // setIsFinish(true);        
+    const [status, setStatus] = useState(Status.Start);
+    const [pazzleWords, setPazzleWords] = useState(words);
+    const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
+    const [currentWordIndex, setCurrentWordIndex] = useState(0);
+    const [isFinish, setIsFinish] = useState(false);
+    const [statstic, setStatistic] = useState(defaultStatistic);
+
+    const handleNext = (result: boolean) => {       
+        const wordId =pazzleWords[currentWordIndex].id; 
+        if (statstic[wordId]) {
+            const newResults = {...statstic};
+            newResults[wordId] = result;
+            setStatistic(newResults);
+        }
+
+        if (currentWordIndex === words.length - 1) {
+            setPazzleWords(shuffle(pazzleWords));
+            setCurrentWordIndex(0);
+            setCurrentQuizIndex(currentQuizIndex + 1);
+            return;
+        }
+        setCurrentWordIndex(currentWordIndex + 1);      
     }
 
-    // useAsyncEffect(async () => {
-    //     if (isFinish) {
-    //         const updatedWords = words.map((word) => {
-    //             word.attempts += 1;
-    //             if (!unsuccessfull[word.id]) {
-    //                 word.successfulAttempts += 1;
-    //                 word.lastSuccessful = new Date ();
-    //             } 
-    //             return word;
-    //         })
-    //     await saveStatistic(updatedWords);
-    //     }
-    // }, [isFinish]);
+    useEffect(()=>{
+        if (currentQuizIndex === quizes.length - 1) {
+            setIsFinish(true);
+        }
+    }, [currentQuizIndex])
 
-    // const CurrentQuiz = quizes[currentQuizIndex];
+    useAsyncEffect(async () => {
+        if (!isFinish) return;
+        const updatedWords = words.map((word) => {
+            word.attempts += 1;
+            if (statstic[word.id]) {
+                word.successfulAttempts += 1;
+                word.lastSuccessful = new Date ();
+            } 
+            return word;
+        })
+        await saveStatistic(updatedWords);
+        setIsFinish(false);
+        setStatus(Status.End);
+    }, [isFinish]);
 
-    return (
-        <QuizCard>
-            {/* <CurrentQuiz words={words} next={handleNext}/> */}
-            {/* <QuizTranslate words={words} next={handleNext}/> */}
-            {/* <QuizSpell words={words} next={handleNext}/> */}
-            <QuizListen words={words} next={handleNext}/>
-        </QuizCard>
-    );
+    const CurrentQuiz = quizes[currentQuizIndex];
+    switch (status) {
+        case Status.Start:
+            return <CurrentQuiz key={pazzleWords[currentWordIndex].word} pazzleWord={pazzleWords[currentWordIndex]} next={handleNext}/>;
+            // return <StartCard onClick={()=>{}/>
+        case Status.End:
+            return <CurrentQuiz key={pazzleWords[currentWordIndex].word} pazzleWord={pazzleWords[currentWordIndex]} next={handleNext}/>;
+            // return <EndCard onClick={()=>{}}/>
+        default:
+            return <CurrentQuiz key={pazzleWords[currentWordIndex].word} pazzleWord={pazzleWords[currentWordIndex]} next={handleNext}/>;
+    }
 }
