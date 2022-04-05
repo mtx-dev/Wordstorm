f80d235b52ab06d4c48c9babff8e33769d7f5e32
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-04-05 23:06:47 +0300

    Movo quizes

--- client/src/hoocks/useQuizes.tsx
+++ client/src/hoocks/useQuizes.tsx
@@ -1,13 +1,24 @@
 import { useContext } from 'react';
-import QuizTranslate from '../components/QuizTranslate';
+import QuizListen from '../components/Quizes/QuizListen';
+import QuizReverseTranslate from '../components/Quizes/QuizReverseTranslate';
+import QuizSpell from '../components/Quizes/QuizSpell';
+import QuizTranslate from '../components/Quizes/QuizTranslate';
+// import QuizTranslate from '../components/QuizTranslate';
 import { Context, StoreContextType } from '../context/Context';
+import { QuizType } from '../models/IQuiz';
 
+const quizes2: QuizType[] = [
+	// QuizTranslate,
+	// QuizReverseTranslate,
+	QuizListen,
+	QuizSpell,
+]
 
-const useQuizes = (): JSX.Element[] => {
+const useQuizes = (): QuizType[] => {
     const { user }= useContext<StoreContextType>(Context);
 	// TODO  filter y name or voice ...
 	// getQuizes().filter((q)=>console.log(q))	
-    return [QuizTranslate];
+    return quizes2;
 }
 
 export default useQuizes;
