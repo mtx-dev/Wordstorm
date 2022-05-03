e15e7bacb26e8147f15606d9624f2b2ac5fc6cb9
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-05-04 00:07:46 +0300

    Fix bug undefined

--- client/src/utils/shuffle.ts
+++ client/src/utils/shuffle.ts
@@ -1,9 +1,8 @@
 import { randomIndex as random } from './random';
 
 export const shuffle = (array: any[]) => {
-	let currentIndex = array.length;
+	let currentIndex = array.length - 1;
 	let randomIndex;
-  
 	while (currentIndex !== 0) {
 		randomIndex = random(currentIndex);
 		currentIndex--;
