629ad86b7ba1abdad9dc07d6de849f3e4eeefcef
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-05-04 00:51:01 +0300

    fix code style

--- client/src/sections/MainSection.tsx
+++ client/src/sections/MainSection.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import { Button, Col, Row } from 'react-bootstrap';
-import UserService from '../services/UserServoce';
+import { Button, Col } from 'react-bootstrap';
 
 
 export default function MainSection() {
     const navigate = useNavigate();
-    const handl = async () => {
-        try {
-            const users = await UserService.getUsers();
-            console.log(users.data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
     const handleStart = () => navigate("/scud");
     return (
         <Col className='d-flex flex-column align-items-center justify-content-center'>
