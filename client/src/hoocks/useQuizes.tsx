40049b57c20b4cc1d25a6aef395ed5648fe72d9c
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-05-04 00:14:42 +0300

    Quizes tyoes rework

--- client/src/hoocks/useQuizes.tsx
+++ client/src/hoocks/useQuizes.tsx
@@ -3,22 +3,21 @@ import QuizListen from '../components/Quizes/QuizListen';
 import QuizReverseTranslate from '../components/Quizes/QuizReverseTranslate';
 import QuizSpell from '../components/Quizes/QuizSpell';
 import QuizTranslate from '../components/Quizes/QuizTranslate';
-// import QuizTranslate from '../components/QuizTranslate';
 import { Context, StoreContextType } from '../context/Context';
 import { QuizType } from '../models/IQuiz';
 
-const quizes2: QuizType[] = [
-	// QuizTranslate,
-	// QuizReverseTranslate,
-	QuizListen,
-	QuizSpell,
-]
+export type QuizNameType = 'Translate' | 'ReverseTranslate' | 'Listen' | 'Spell';
+const quizes: Record <QuizNameType, QuizType> = {
+	'Translate': QuizTranslate,
+    'ReverseTranslate': QuizReverseTranslate,
+    'Listen': QuizListen,
+    'Spell': QuizSpell,
+}
 
 const useQuizes = (): QuizType[] => {
     const { user }= useContext<StoreContextType>(Context);
-	// TODO  filter y name or voice ...
-	// getQuizes().filter((q)=>console.log(q))	
-    return quizes2;
+    return user.settings.quizes.map((quizName) => quizes[quizName]);
 }
+
 
 export default useQuizes;
