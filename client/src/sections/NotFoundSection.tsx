93bfa80af056d09d92d95b028fbd86ed45cfcfaa
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-05-04 00:51:14 +0300

    Rework 404 page

--- client/src/sections/NotFoundSection.tsx
+++ client/src/sections/NotFoundSection.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Col, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFoundSection() {
+    const navigate = useNavigate();
+
+    const handleGoBack = () => navigate(-1);
+
     return (
-        <div>NotFoundSection</div>
+        <Col className='d-flex flex-column align-items-center justify-content-center'>
+            <h1>404</h1>
+            <h2>Not Found</h2>
+            <h2>{':-('}</h2>
+            <Button onClick={handleGoBack} variant='outline-secondary'>back</Button>
+        </Col>
     );
 }
