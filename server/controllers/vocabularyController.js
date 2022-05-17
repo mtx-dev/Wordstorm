c43df66fca8a1cb397837ec75eab10a75a6713a0
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-05-17 22:41:58 +0300

    Add controllers

---
+++ server/controllers/vocabularyController.js
@@ -0,0 +1,68 @@
+const userService = require('../service/user-service');
+const ApiError = require('../exeptions/api-error');
+
+class VocabularyController {
+    async getVocabulary(req, res, next) {
+        try {
+			// TODO cheack on norm str
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Error validation', errors));
+            }
+            const {email, password} = req.body;
+            const userData = await userService.registration(email, password);
+
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 3600 * 1000, httpOnly: true})
+            console.log('======== reg');
+
+            return res.json(userData);
+        } catch(error) {
+            next(error);
+        }
+    }
+
+    async addWord(req, res, next) {
+        try {
+			// TODO cheack on norm str
+
+            // const {email, password} = req.body;
+            // const userData = await userService.login(email, password);
+            // res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 3600 * 1000, httpOnly: true})
+            // console.log('======== login');
+            // return res.json(userData);
+        } catch(error) {
+            next(error);
+        }
+    }
+
+    async update(req, res, next) {
+        try {
+			// TODO cheack on norm str
+
+            // const {refreshToken} = req.cookies;
+            // const token = await userService.logout(refreshToken);
+            // res.clearCookie('refreshToken');
+            // console.log('======== logout');
+            // res.json(token);
+        } catch(error) {
+            next(error);
+        }
+    }
+
+	async updates(req, res, next) {
+        try {
+			// TODO cheack on norm str
+
+            // const {refreshToken} = req.cookies;
+            // const token = await userService.logout(refreshToken);
+            // res.clearCookie('refreshToken');
+            // console.log('======== logout');
+            // res.json(token);
+        } catch(error) {
+            next(error);
+        }
+    }
+}
+
+
+module.exports = new VocabularyController();
--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,6 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user-controller');
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 const authMiddleware = require('../middlewares/auth-middleware');
 
 const router = new Router();
@@ -14,5 +14,14 @@ router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
 router.get('/users', authMiddleware, userController.getUsers);
+
+router.get('/dictionaryary/search', authMiddleware, dictionaryController.search);
+router.get('/dictionaryary/fake-words', authMiddleware, dictionaryController.fakeWords);
+router.get('/dictionaryary/fake-translations', authMiddleware, dictionaryController.fakeTranslations);
+
+router.get('/vocabulary', authMiddleware, vocabularyController.getVocabulary);
+router.post('/vocabulary/add', authMiddleware, vocabularyController.addWord);
+router.patch('/vocabulary/update', authMiddleware, vocabularyController.update);
+router.put('/vocabulary/updates', authMiddleware, vocabularyController.updates);
 
 module.exports = router;
